1d581d194bea78d22d10bc6b07845c869eb66c32
Robert DeVore <deviodigital@gmail.com>
2025-02-01 19:38:40 -0500

    Initial commit

--- telemetry-tracker.php
+++ telemetry-tracker.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Telemetry Tracker
  * Description: Tracks active installs for your plugins via WordPress REST API and provides analytics.
- * Version: 1.1.1
+ * Version: 1.0.0
  * Author: Robert DeVore
  * Author URI: https://robertdevore.com/
  * License: GPL-2.0+
@@ -17,7 +17,10 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 use TelemetryTracker\TelemetryTracker;
 
-$telemetry_tracker = new TelemetryTracker();
+$plugin_slug    = 'telemetry-tracker';
+$plugin_version = '1.0.0';
+
+$telemetry_tracker = new TelemetryTracker( $plugin_slug, $plugin_version );
 
 register_activation_hook( __FILE__, [ 'TelemetryTracker\\TelemetryTracker', 'activate' ] );
 register_deactivation_hook( __FILE__, [ 'TelemetryTracker\\TelemetryTracker', 'deactivate' ] );
---
+++ vendor/composer/installed.php
@@ -0,0 +1,44 @@
+<?php return array(
+    'root' => array(
+        'name' => 'robertdevore/telemetry-tracker',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'c03dc72548edf5e98b74c623110bf4a8427cd09c',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'composer/installers' => array(
+            'pretty_version' => 'v1.12.0',
+            'version' => '1.12.0.0',
+            'reference' => 'd20a64ed3c94748397ff5973488761b22f6d3f19',
+            'type' => 'composer-plugin',
+            'install_path' => __DIR__ . '/./installers',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'robertdevore/telemetry-tracker' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'c03dc72548edf5e98b74c623110bf4a8427cd09c',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'roundcube/plugin-installer' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '*',
+            ),
+        ),
+        'shama/baton' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '*',
+            ),
+        ),
+    ),
+);
